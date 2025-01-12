a20ccd4220223c705218eae0fff3f1ed7eb41e4b
kartavya <kartavyasandhu1@gmail.com>
2025-01-12 23:01:43 +0530

    Theme switcher completed with context API

---
+++ themeSwitcher/src/contexts/theme.js
@@ -0,0 +1,14 @@
+import React, { useContext } from "react";
+
+
+export const ThemeContext = React.createContext({
+    themeMode : "light",
+    darkTheme : () => {},
+    lightTheme : () => {}
+})
+
+export const ThemeProvider = ThemeContext.Provider;
+
+export default function useTheme(){
+    return useContext(ThemeContext)
+}
