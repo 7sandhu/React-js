ebe1f65a639741bbbb8e89f7b6dd7796d6ea0e12
kartavya <kartavyasandhu1@gmail.com>
2025-01-13 10:24:15 +0530

    complete Todo with local storage and context api

---
+++ toDoContextLocalStorage/src/contexts/TodoContext.js
@@ -0,0 +1,25 @@
+import React ,{ useContext } from "react";
+
+
+export const TodoContext = React.createContext({
+
+    todos : [
+        {
+            id : 1,
+            todo : "Todo msg",
+            completed : false
+        }
+    ],
+    addTodo : (todo) => {},
+    updateTodo : (id,todo) => {},
+    deleteTodo : (id) => {},
+    toggleComplete : (id) => {}
+
+});
+
+
+export const useTodo = () => {
+    return useContext(TodoContext);
+}
+
+export const TodoProvider = TodoContext.Provider;
