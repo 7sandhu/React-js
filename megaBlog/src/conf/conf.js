e93a115367b7ecc6b7fa3373f3c732c62b7ea846
kartavya <kartavyasandhu1@gmail.com>
2025-05-27 22:31:58 +0530

    almost complete megablog

---
+++ megaBlog/src/conf/conf.js
@@ -0,0 +1,12 @@
+const conf = {
+
+    appwriteUrl : String(import.meta.env.VITE_APPWRITE_URL) ,
+    appwriteDatabaseId : String(import.meta.env.VITE_APPWRITE_DATABASE_ID) ,
+    appwriteCollectionId : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID) ,
+    appwriteBucketId : String(import.meta.env.VITE_APPWRITE_BUCKET_ID) ,
+    appwriteProjectId : String(import.meta.env.VITE_APPWRITE_PROJECT_ID)
+
+
+}
+
+export default conf
